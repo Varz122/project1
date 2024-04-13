b125d3063be6de5fa789a6c7225ec2771cb1461f
Varz122 <154825567+Varz122@users.noreply.github.com>
2024-04-13 19:34:32 +0530

    Add files via upload

--- router.js
+++ router.js
@@ -1,7 +1,12 @@
-const http = require('http');
-const route = require('./route')
+const express = require('express')
+const app = express()
+app.use((req,res,next)=>{
+    console.log("this is a middleware");
+    next();
+})
+app.use((req,res,next)=>{
+    console.log("this is another middleware");
+  res.send("<h2> Welcome to Express.js </h2>")
+})
 
-
-const server = http.createServer(route)
-
-server.listen(7000);
+app.listen(7000)
